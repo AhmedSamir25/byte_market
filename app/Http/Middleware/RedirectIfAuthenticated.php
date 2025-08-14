24e5b956821fb102ff7678674492adc1f2be9cb7
Ahmed Samir <ahmed_samir3@outlook.com>
2025-08-15 00:40:19 +0300

    Authenticated Session Controller and Login Request

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class RedirectIfAuthenticated
 {
@@ -22,7 +23,7 @@ class RedirectIfAuthenticated
         $guards = empty($guards) ? [null] : $guards;
 
         foreach ($guards as $guard) {
-            if (Auth::guard($$guard)->check()) {
+            if (Auth::guard($guard)->check()) {
                 return redirect($this->redirectTo($request, $guard));
             }
         }
