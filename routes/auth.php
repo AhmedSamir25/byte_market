b4d2082352e4be7e5dca5a20307a3ac32c1eeeba
Ahmed Samir <ahmed_samir3@outlook.com>
2025-08-15 01:05:09 +0300

    Reset password controller

--- routes/auth.php
+++ routes/auth.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,4 +17,22 @@ Route::middleware("guest")->group(function () {
         "create",
     ])->name("login");
     Route::post("login", [AuthenticatedSessionController::class, "store"]);
+
+    Route::get("forgot-password", [
+        PasswordResetController::class,
+        "create",
+    ])->name("password.request");
+    Route::post("forgot-password", [
+        PasswordResetController::class,
+        "store",
+    ])->name("password.email");
+    Route::get("reset-password/{token}", [
+        NewPasswordController::class,
+        "create",
+    ])->name("password.reset");
+
+    Route::post("reset-password", [
+        NewPasswordController::class,
+        "store",
+    ])->name("password.store");
 });
--- routes/web.php
+++ routes/web.php
@@ -7,27 +7,6 @@ Route::get("/", function () {
     return view("welcome");
 });
 
-Route::get("/register", function () {
-    return view("auth.register");
-})->name("register");
-
-Route::post("/register", function () {});
-
-Route::get("/forgot-password", function () {
-    return view("auth.forgot-password");
-})->name("password.request");
-
-Route::post("/forgot-password", function () {
-    // Password reset logic
-    request()->validate(["email" => "required|email"]);
-
-    // Here you would typically send a password reset email
-    return back()->with(
-        "status",
-        "A password reset link has been sent to your email.",
-    );
-})->name("password.email");
-
 Route::post("/logout", function () {
     Auth::logout();
     request()->session()->invalidate();
